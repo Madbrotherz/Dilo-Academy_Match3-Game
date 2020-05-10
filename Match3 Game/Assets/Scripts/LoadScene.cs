111b126dd35e3fe9748459db35b594ff9a0462a4
Madbrotherz <61371417+Madbrotherz@users.noreply.github.com>
2020-05-10 19:02:25 +0700

    Commit & Push to Master Branch

---
+++ Match3 Game/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public GameObject pausePanel;
+    public GameObject resumeBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Loadscenes(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        resumeBtn.SetActive(false);
+    }
+}
---
+++ Match3 Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float currentTime = 0f;
+    public float startTime = 10f;
+    public bool timesUp = false;
+    public GameObject pausePanel;
+
+    public Text CountDownText;
+    void Start()
+    {
+        currentTime = startTime;
+        timesUp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime > 0 && !timesUp)
+        {
+            currentTime -= 1 * Time.deltaTime;
+            print(currentTime);
+            CountDownText.text = currentTime.ToString("0");
+            if (currentTime <= 0)
+            {
+                gameOver();
+            }
+        }
+
+        if (currentTime >= 3.5f) { CountDownText.color = Color.black; }
+        if (currentTime < 3.5f) { CountDownText.color = Color.red; }
+
+
+    }
+
+    public void gameOver()
+    {
+        Time.timeScale = 0;
+        timesUp = true;
+        pausePanel.SetActive(true);
+    }
+}
